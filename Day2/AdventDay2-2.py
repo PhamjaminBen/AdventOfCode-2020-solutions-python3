fb377a7715eb6ca67596b2f52e38a6c1f3df1853
Ben Pham <69863256+PhamjaminBen@users.noreply.github.com>
2020-12-07 19:15:49 -0800

    Update AdventDay2-2.py

--- Day2/AdventDay2-2.py
+++ Day2/AdventDay2-2.py
@@ -1,12 +1,15 @@
 fin = open('adventdata.in','r')
+#extracting passwords
 code = [x.strip().split(":") for x in fin]
+#counts valid passwords
 count = 0
 for x in code:
+	#text proscessing
 	arr = x[0].split("-")
 	minimum = int(arr[0])
 	maximum = int(arr[1][0:arr[1].find(" ")])
 	letter = arr[1][-1]
-	print(f'{minimum}:{maximum}:{letter}:{x[1]}')
+	#problem requires that the letter appears at either the maximum index OR minmum index but not both
 	if(x[1][minimum] == letter and x[1][maximum] != letter):	
 		count = count + 1
 		print("VALID")
